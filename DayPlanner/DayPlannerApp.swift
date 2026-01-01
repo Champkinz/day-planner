bf8c9ee1604aef8166acc1ccd6ec295e266146fe
Charana <charana.am@gmail.com>
2026-01-01 15:54:32 +0530

    Initial Commit

---
+++ DayPlanner/DayPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayPlannerApp.swift
+//  DayPlanner
+//
+//  Created by Charana Amarasekara on 2026-01-01.
+//
+
+import SwiftUI
+
+@main
+struct DayPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
